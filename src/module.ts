38f59b116a97548eaa288d2330c06c35a488ee7a
Florian Weber <fl-weber@t-online.de>
2022-07-23 17:38:40 +0200

    feat: added support for maxAge and cacheControl options

--- src/module.ts
+++ src/module.ts
@@ -7,6 +7,8 @@ import { virtual } from './virtual'
 export type ViteCompressionOptions = Parameters<typeof viteCompression>[0];
 
 export interface ModuleOptions {
+  maxAge?: number | ((path: string) => number);
+  cacheControl?: string | ((path: string) => number);
   viteCompression?: ViteCompressionOptions;
 }
 
@@ -16,6 +18,7 @@ export default defineNuxtModule<ModuleOptions>({
     configKey: 'compression'
   },
   defaults: {
+    maxAge: 60 * 60 * 24 * 365,
     viteCompression: {
       algorithm: 'brotliCompress',
       filter: /\.(js|mjs|json|css|html)$/i
@@ -44,6 +47,16 @@ export default defineNuxtModule<ModuleOptions>({
     })
 
     nuxt.hook('nitro:build:before', (nitro) => {
+      let cacheControlHeader
+
+      if (options.cacheControl) {
+        const cacheControl = typeof options.cacheControl === 'function' ? 'cacheControl(assetPath)' : 'cacheControl'
+        cacheControlHeader = `res.setHeader("Cache-Control", ${cacheControl});`
+      } else {
+        const maxAge = typeof options.maxAge === 'function' ? 'maxAge(assetPath)' : 'maxAge'
+        cacheControlHeader = `res.setHeader("Cache-Control", \`max-age=\${${maxAge}}, immutable\`);`
+      }
+
       nitro.options.rollupConfig?.plugins?.push(
         virtual(
           {
@@ -54,19 +67,23 @@ import { dirname } from 'pathe'
 import { fileURLToPath } from 'url'
 import assets from '#internal/nitro/virtual/public-assets-data'
 
-${typeof filter === 'function' ? `const filterFunction = ${filter.toString()}` : ''}
-
+const maxAge = ${options.maxAge};
+const cacheControl = ${typeof options.cacheControl === 'function' ? options.cacheControl.toString() : options.cacheControl ? `'${options.cacheControl}'` : 'undefined'};
+const filter = ${filter};
+ 
 export async function readAsset (id, res) {
   const serverDir = dirname(fileURLToPath(import.meta.url))
   let assetPath = resolve(serverDir, assets[id].path)
 
-  if (${typeof filter === 'function' ? 'filterFunction(assetPath)' : '/\\.(js|mjs|json|css|html)$/i.test(assetPath)'}) {
+  if (${typeof filter === 'function' ? 'filter(assetPath)' : 'filter.test(assetPath)'}) {
     try {
       await fsp.access(\`\${assetPath}.${fileExtension}\`, constants.R_OK | constants.W_OK);
       assetPath = \`\${assetPath}.${fileExtension}\`;
       res.setHeader('Content-Encoding', '${contentEncoding}');
     } catch {}
   }
+
+  ${cacheControlHeader}
 
   return fsp.readFile(assetPath);
 }`
