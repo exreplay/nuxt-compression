18d50df4a0de192946eb359622c98fc6285a0d26
Florian Weber <fl-weber@t-online.de>
2022-08-01 15:35:17 +0200

    chore: added tests

--- src/module.ts
+++ src/module.ts
@@ -3,12 +3,13 @@ import { defineNuxtModule } from '@nuxt/kit'
 import viteCompression from 'vite-plugin-compression'
 import { globbySync } from 'globby'
 import { virtual } from './virtual'
+import { getPublicAssets } from './utils'
 
 export type ViteCompressionOptions = Parameters<typeof viteCompression>[0];
 
 export interface ModuleOptions {
   maxAge?: number | ((path: string) => number);
-  cacheControl?: string | ((path: string) => number);
+  cacheControl?: string | ((path: string) => string);
   viteCompression?: ViteCompressionOptions;
 }
 
@@ -25,21 +26,6 @@ export default defineNuxtModule<ModuleOptions>({
     }
   },
   setup (options, nuxt) {
-    const filter = options.viteCompression.filter
-    let fileExtension = ''
-    let contentEncoding = ''
-
-    switch (options.viteCompression.algorithm) {
-      case 'gzip':
-        fileExtension = 'gz'
-        contentEncoding = 'gzip'
-        break
-      case 'brotliCompress':
-        fileExtension = 'br'
-        contentEncoding = 'br'
-        break
-    }
-
     nuxt.hook('vite:extend', (context) => {
       context.config.plugins?.push(
         viteCompression(options.viteCompression)
@@ -47,46 +33,10 @@ export default defineNuxtModule<ModuleOptions>({
     })
 
     nuxt.hook('nitro:build:before', (nitro) => {
-      let cacheControlHeader
-
-      if (options.cacheControl) {
-        const cacheControl = typeof options.cacheControl === 'function' ? 'cacheControl(assetPath)' : 'cacheControl'
-        cacheControlHeader = `res.setHeader("Cache-Control", ${cacheControl});`
-      } else {
-        const maxAge = typeof options.maxAge === 'function' ? 'maxAge(assetPath)' : 'maxAge'
-        cacheControlHeader = `res.setHeader("Cache-Control", \`max-age=\${${maxAge}}, immutable\`);`
-      }
-
       nitro.options.rollupConfig?.plugins?.push(
         virtual(
           {
-            '#internal/nitro/virtual/public-assets-node': `
-import { promises as fsp, constants } from 'fs'
-import { resolve } from 'pathe'
-import { dirname } from 'pathe'
-import { fileURLToPath } from 'url'
-import assets from '#internal/nitro/virtual/public-assets-data'
-
-const maxAge = ${options.maxAge};
-const cacheControl = ${typeof options.cacheControl === 'function' ? options.cacheControl.toString() : options.cacheControl ? `'${options.cacheControl}'` : 'undefined'};
-const filter = ${filter};
- 
-export async function readAsset (id, res) {
-  const serverDir = dirname(fileURLToPath(import.meta.url))
-  let assetPath = resolve(serverDir, assets[id].path)
-
-  if (${typeof filter === 'function' ? 'filter(assetPath)' : 'filter.test(assetPath)'}) {
-    try {
-      await fsp.access(\`\${assetPath}.${fileExtension}\`, constants.R_OK | constants.W_OK);
-      assetPath = \`\${assetPath}.${fileExtension}\`;
-      res.setHeader('Content-Encoding', '${contentEncoding}');
-    } catch {}
-  }
-
-  ${cacheControlHeader}
-
-  return fsp.readFile(assetPath);
-}`
+            '#internal/nitro/virtual/public-assets-node': getPublicAssets(options)
           },
           nitro.vfs
         ) as never
